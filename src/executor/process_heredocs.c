c38320f2afe930570ef395f7bd1921a88a22cd2b
Ari <aril.abcmail@gmail.com>
2025-06-03 23:12:17 +0200

    feat(executor): implementar señales correctamente en heredoc
    - Se resolvió el problema con el manejo de señales durante la ejecución de heredoc.
    - Ahora Ctrl+C interrumpe correctamente la entrada sin dejar la shell en estado inconsistente.
    - Comportamiento normal restaurado tras la señal.

--- src/executor/process_heredocs.c
+++ src/executor/process_heredocs.c
@@ -6,7 +6,7 @@
 /*   By: abaldelo <abaldelo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 00:04:23 by abaldelo          #+#    #+#             */
-/*   Updated: 2025/06/03 21:39:08 by abaldelo         ###   ########.fr       */
+/*   Updated: 2025/06/03 23:06:32 by abaldelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,19 +29,15 @@ static bool	heredoc_child(t_shell *shell, t_cmd *cmd, int pip[2])
 			exit(EXIT_KO);
 		exit(EXIT_OK);
 	}
-	else
+	ignore_child_signals();
+	waitpid(pid, &status, 0);
+	if (WIFSIGNALED(status) && WTERMSIG(status) == SIGINT)
 	{
-		ignore_child_signals();
-		waitpid(pid, &status, 0);
-		if (WIFSIGNALED(status) && WTERMSIG(status) == SIGINT)
-		{
-			ft_putstr_fd("\n", STDOUT_FILENO);
-			shell->last_exit = 130;
-			return (false);
-		}
-		if (!WIFEXITED(status) || WEXITSTATUS(status) != EXIT_OK)
-			return (false);
+		ft_putstr_fd("\n", STDOUT_FILENO);
+		return (setup_shell_signals(), shell->last_exit = 130, false);
 	}
+	if (!WIFEXITED(status) || WEXITSTATUS(status) != EXIT_OK)
+		return (false);
 	return (true);
 }
 
