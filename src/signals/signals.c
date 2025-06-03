4758bbe7dcaca4862312e0a52aa59fdf9650fd65
Beatriz Gil Fernandez <bgil-fer@student.42madrid.com>
2025-06-03 13:27:44 +0200

    Algunas modificaciones en el archivo signals.c Aun no está terminado.

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abaldelo <abaldelo@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: bgil-fer <bgil-fer@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 22:52:00 by abaldelo          #+#    #+#             */
-/*   Updated: 2025/05/22 12:00:26 by abaldelo         ###   ########.fr       */
+/*   Updated: 2025/06/03 13:27:02 by bgil-fer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,21 @@
 
 void	handler_sigint(int sig)
 {
-	g_signal = sig;
+	g_signal = sig; // Para qué?
+
+	if (g_signal == 1) //hace falta?
+		return ;
+	printf("\nminishell> ");
+	g_signal = 1;
+	// printf("\n"); Estas cuatro líneas creo que hacen lo mismo que la anterior
+	// rl_replace_line("", 1);
+	// rl_on_new_line();
+	// rl_redisplay();
 }
 
 void	setup_signals(void)
 {
-	signal(SIGINT, handler_sigint);
-	signal(SIGQUIT, SIG_IGN);
+	signal(SIGINT, handler_sigint); //Ctrl-C
+	signal(SIGQUIT, SIG_IGN);//para el Ctrl-\.
+	//ctrl-D por sistema se interpreta como EOF y esto, en nuestro programa lo cierra :)
 }
